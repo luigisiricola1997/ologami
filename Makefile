3092f305f20c1fcfef73de01d82cb316432ab7eb
Luigi Siricola <l.siricola@nexidigital.eu>
2023-10-25 15:05:52 +0200

    feat: add ologami backend which receive error logs from nodejs-logger and log it

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ local: build deploy-local
 
 build:
 	docker build -t nodejs-logger nodejs-logger/ --no-cache
+	docker build -t ologami-backend ologami/backend/ --no-cache
 
 deploy-local:
 	kubectl delete -f k8s/nodejs-logger.yaml || true
+	kubectl delete -f k8s/ologami-backend.yaml || true
 	kubectl apply -f k8s/nodejs-logger.yaml
+	kubectl apply -f k8s/ologami-backend.yaml
--- nodejs-logger/nodejs-logger.js
+++ nodejs-logger/nodejs-logger.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path');
+const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Funzione per loggare su console
 const logMessage = (message) => {
@@ -13,9 +13,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   const errorMessage = `Errore 404: Pagina ${req.url} non trovata`;
   logMessage(errorMessage);
+
+  // Invia il log a Ologami
+  try {
+    await axios.post('http://ologami-backend-service:3000/log', {
+      message: errorMessage,
+      type: 'error',
+      timestamp: new Date().toISOString()
+    });
+  } catch (error) {
+    console.error('Errore durante l\'invio del log:', error);
+  }
+
   res.status(404).send(errorMessage);
 });
 
