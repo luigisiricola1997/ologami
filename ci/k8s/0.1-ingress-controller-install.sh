d3db0219df4a836cbe94fcc10f04af11293072eb
Luigi Siricola <l.siricola@nexidigital.eu>
2024-11-26 23:52:28 +0100

    improved Makefile, fix svc types, add ingress-controller, fix init-mongo probe

--- ci/k8s/0.1-ingress-controller-install.sh
+++ ci/k8s/0.1-ingress-controller-install.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 export PATH=$PATH:/c/Program\ Files/Git/bin
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
