ceff34e96dea1c55562aef2bd19bc02a557468e4
Luigi Siricola <luigi.siricola1997@outlook.it>
2025-04-25 17:53:08 +0200

    refactor: streamline ingress controller installation script by removing conditional checks and adding repo commands

--- ci/k8s/0.1-create-ingress-controller.sh
+++ ci/k8s/0.1-create-ingress-controller.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# export PATH=$PATH:/c/Program\ Files/Git/bin:/C/Program\ Files/Git/bin/helm.exe
-
-if ! helm status ingress-nginx -n ingress-nginx >/dev/null 2>&1; then
-    echo "Installing ingress controller..."
-    helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
-fi
-
-
-# To update:
-# echo "Ingress controller already installed, upgrading..."
-# helm upgrade ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace || true
